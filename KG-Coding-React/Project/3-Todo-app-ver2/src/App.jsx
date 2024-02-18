aa511ee6a26fb3fc362e935b6083b13ca590216a
chinmaya-kumar-das <chinmayakumardas2000@gmail.com>
2024-02-18 14:15:15 +0530

    add

--- KG-Coding-React/Project/3-Todo-app-ver2/src/App.jsx
+++ KG-Coding-React/Project/3-Todo-app-ver2/src/App.jsx
@@ -3,27 +3,26 @@ import AddTodo from "./components/AddTodo";
 import Appname from "./components/Appname";
 import TodoItems from "./components/TodoItems";
 
-
 function App() {
-  let todoItems=[
+  let todoItems = [
     {
-      name:"Go to market for fish",
-        dueDate:"2/1/2023"
-  },
-  {
-    name:"Go to  college exam ",
-      dueDate:"2/2/2023"
-},
-  {
-    name:"Go to  Army hospital ",
-      dueDate:"3/2/2023"
-}];
+      name: "Go to market for fish",
+      dueDate: "2/1/2023",
+    },
+    {
+      name: "Go to  college exam ",
+      dueDate: "2/2/2023",
+    },
+    {
+      name: "Go to  Army hospital ",
+      dueDate: "3/2/2023",
+    },
+  ];
   return (
     <center className="todo-container">
       <Appname />
       <AddTodo />
       <TodoItems todoItems={todoItems}></TodoItems>
-      
     </center>
   );
 }
