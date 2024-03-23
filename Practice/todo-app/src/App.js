7df324d60684addedc7873898627cf7e558bfaff
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-03-24 00:21:33 +0530

    add

---
+++ Practice/todo-app/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App bg-slate-700">3e3e3e</div>;
+}
+
+export default App;
---
+++ Practice/todo-app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['.src/**/*.{xml,js,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
