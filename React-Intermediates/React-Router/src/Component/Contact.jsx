d888155709b8642f04eccde14fdccf116d2e895b
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-04-10 20:59:01 +0530

    add

--- React-Intermediates/React-Router/src/Component/Contact.jsx
+++ React-Intermediates/React-Router/src/Component/Contact.jsx
@@ -8,6 +8,7 @@ const Contact = () => {
   return (
     <div>
       
+      
       Mob: 6370073215
       <br></br>
       <button onClick={clickHandler} className='bg-red-400 px-4 rounded-sm'>Move to About Page</button>
