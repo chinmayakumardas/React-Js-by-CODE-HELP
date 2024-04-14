d2117cb8a02805d7ac4c953bddce4dda598e1914
CHINMAYA KUMAR DAS <115868110+chinmaya-kumar-das@users.noreply.github.com>
2024-04-14 15:45:21 +0000

    dfd

--- React-Intermediates/studynotion1/src/App.js
+++ React-Intermediates/studynotion1/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" elemenet={<Home />} />
+        <Route path="" elemenet={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
