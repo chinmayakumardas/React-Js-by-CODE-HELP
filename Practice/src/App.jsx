7e3eacc70c3143e6f452220c7a4807fa96a2a5a3
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-04-22 02:53:35 +0530

    added

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cards from './Card_Component/Cards';
 
 
-const NetaList = [
-  { name: 'Modi', id: 1,url:'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-560w,f_auto,q_auto:best/rockcms/2023-01/230127-narendra-modi-mb-0952-6da744.jpg' },
-  { name: 'Naveen', id: 2,url:'https://t0.gstatic.com/images?q=tbn:ANd9GcQgyTGPOUHYkFEhrfpmgV_6U4Kmar-4acvj8z3zhVR-z7BwkXp9' },
-  { name: 'Kejirwal', id: 3 ,url:'https://c.ndtvimg.com/2023-01/f30097jo_arvind-kejriwal_625x300_06_January_23.jpg?im=FeatureCrop,algorithm=dnn,width=650,height=400'},
-];
 
 const App = () => {
+  
   return (
     <div className="app">
       <h1>Politician List</h1>
-      <Cards NetaList={NetaList}  />
+      <Cards />
       
     </div>
   );
--- Practice/src/Card_Component/Cards.jsx
+++ Practice/src/Card_Component/Cards.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Card from './Card';
 
-const Cards = ({ NetaList }) => {
-   
+const Cards = () => {
+  let [NetaList,setNetaList]=useState(
+    [
+      { name: 'Modi', id: 100,url:'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-560w,f_auto,q_auto:best/rockcms/2023-01/230127-narendra-modi-mb-0952-6da744.jpg' },
+      { name: 'Naveen', id: 101,url:'https://t0.gstatic.com/images?q=tbn:ANd9GcQgyTGPOUHYkFEhrfpmgV_6U4Kmar-4acvj8z3zhVR-z7BwkXp9' },
+      { name: 'Kejirwal', id: 103 ,url:'https://c.ndtvimg.com/2023-01/f30097jo_arvind-kejriwal_625x300_06_January_23.jpg?im=FeatureCrop,algorithm=dnn,width=650,height=400'},
+    ],
+  );
+  function deleteHandler(index){
+    setNetaList((pre)=>{
+        let data=[...pre];
+        data.splice(index,1);
+        return data;
+      })
+  }
   return (
     <div className="cards flex gap-5 px-2 rounded-md border-red-500">
       {NetaList.map((card, index) => (
-        <Card key={index}  name={card.name} id={card.id} url={card.url} />
+        <Card key={index}  name={card.name} id={card.id} url={card.url} deleteHandler={()=> deleteHandler(index)}/>
       ))}
     </div>
   );
